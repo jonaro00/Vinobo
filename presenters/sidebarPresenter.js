4335ce3d0f4fb233c0cf4eb7c990ed7239bbe597
Simon Falk <smnfalk@gmail.com>
2021-12-02 11:15:52 +0100

    Can add URL in sidebar

--- presenters/sidebarPresenter.js
+++ presenters/sidebarPresenter.js
@@ -1,16 +1,21 @@
 import React from "react";
 import useModelProperty from "../js/useModelProperty.js";
 import SidebarView from "../views/sidebarView.js";
-import extractID from "../js/transcript.js";
+import { extractID } from "../js/transcript.js";
 
 export default function SidebarPresenter(props) {
+  const [error, setError] = React.useState(null);
   const videos = useModelProperty(props.model, "videos");
+
   return (
     <SidebarView
       videos={videos}
       videoChoice={(id) => props.model.setCurrentVideo(id)}
       addVideo={(ref) => {
-        props.model.addVideo(ref);
+        if (extractID(ref)) {
+          props.model.addVideo(extractID(ref));
+        }
+        setError("Not referring to a valid ID/URL");
       }}
       removeVideo={(id) => props.model.removeVideo(id)}
     ></SidebarView>
