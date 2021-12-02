000e1627dfaf028b36c7d2cda1f3d34bacdd4146
bramapeters <bramapeters@hotmail.com>
2021-12-02 17:54:16 +0100

    ControlView styling
    ControlView styling

--- views/controlView.js
+++ views/controlView.js
@@ -5,21 +5,15 @@ export default function ControlView(props) {
   return (
     <div>
       <form>
-        <table>
-          <tr>
-            <td>
-              <input type="text" value={props.currentTime} />
-            </td>
-            <td>
-              <input type="submit" value="Add Note" />
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <textarea size="20" type="textarea" value="Note" />
-            </td>
-          </tr>
-        </table>
+        <div className={styles.controlView}>
+          <div>
+            <input className={styles.controlTimeStamp} type="text" value={props.currentTime} />
+            <input className={styles.controlAddNote} type="submit" value="Add Note" />
+          </div>
+          <div>
+            <textarea className={styles.controlBodyElement} type="textarea" value="Note" />
+          </div>
+        </div>
       </form>
     </div>
   );
