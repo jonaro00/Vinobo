62fa5eb0ba5107869922293036d632bce00f8079
jonaro00 <54029719+jonaro00@users.noreply.github.com>
2021-12-03 17:30:21 +0100

    Fix state in NotesPresenter

--- presenters/notesPresenter.js
+++ presenters/notesPresenter.js
@@ -1,6 +1,9 @@
 import NotesView from "../views/notesView.js";
 import React from "react";
+import useModelProperty from "../js/useModelProperty.js";
 
 export default function NotesPresenter(props) {
-  return <NotesView notes={props.model.notes} currentVideo={props.model.currentVideo} />;
+  const id = useModelProperty(props.model, "currentVideo");
+  const videos = useModelProperty(props.model, "videos");
+  return <NotesView notes={props.model.notes} currentVideo={id} />;
 }
