32ae3a9d92c3a3fec378288c3fe74afd46c8cf03
jonaro00 <54029719+jonaro00@users.noreply.github.com>
2021-12-06 19:41:34 +0100

    API warning fixed

--- pages/api/get_transcript.js
+++ pages/api/get_transcript.js
@@ -1,14 +1,14 @@
 import YoutubeTranscript from "youtube-transcript";
 
-export default function handler(req, res) {
+export default async function handler(req, res) {
   const id = new URL(req.url, `http://${req.headers.host}`).searchParams.get("id");
-  YoutubeTranscript.fetchTranscript(id)
+  return YoutubeTranscript.fetchTranscript(id)
     .then((data) => ({
       status: "success",
       data: res.status(200).json(data),
     }))
     .catch((error) => ({
       status: "error",
-      data: res.status(200).json(error),
+      data: res.status(500).json(error),
     }));
 }
