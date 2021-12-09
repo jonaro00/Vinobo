d0c7c7a903735627853345bc54a13120d3eb42a9
bramapeters <bramapeters@hotmail.com>
2021-12-09 15:35:19 +0100

    ControlView AddNote & Clear button
    ControlView AddNote & Clear button

--- views/controlView.js
+++ views/controlView.js
@@ -10,36 +10,31 @@ export default function ControlView(props) {
         }}
       >
         <div className={styles.controlView}>
-          <div>
-            <input
-              className={styles.controlTimeStamp}
-              onChange={(ref) => props.setTimestamp(ref)}
-              name="timestamp"
-              type="appt-time"
-              placeholder={props.currentTime}
-            />
-            <input className={styles.controlAddNote} type="submit" value="Add Note" />
-          </div>
-          <div>
-            <input
-              className={styles.controlTitleElement}
-              onChange={(ref) => props.setTitle(ref)}
-              name="title"
-              type="input"
-              placeholder="Title"
-              required
-            />
-          </div>
-          <div>
-            <textarea
-              className={styles.controlNoteElement}
-              onChange={(ref) => props.setContent(ref)}
-              name="note"
-              type="textarea"
-              placeholder="Note"
-              required
-            />
-          </div>
+          <input
+            className={styles.controlTimeStamp}
+            onChange={(ref) => props.setTimestamp(ref)}
+            name="timestamp"
+            type="appt-time"
+            placeholder={props.currentTime}
+          />
+          <input type="reset" className={styles.controlClear} value="Clear" />
+          <input
+            className={styles.controlTitleElement}
+            onChange={(ref) => props.setTitle(ref)}
+            name="title"
+            type="input"
+            placeholder="Title"
+            required
+          />
+          <input className={styles.controlAddNote} type="submit" value="Add Note" />
+          <textarea
+            className={styles.controlNoteElement}
+            onChange={(ref) => props.setContent(ref)}
+            name="note"
+            type="textarea"
+            placeholder="Note"
+            required
+          />
         </div>
       </form>
     </div>
