10264af10b3d8946ea4febbe53217f611fa6d09c
Simon Falk <smnfalk@gmail.com>
2021-12-10 14:27:38 +0100

    No code repetition, only one presenter for signin/register

--- presenters/signinPresenter.js
+++ presenters/signinPresenter.js
@@ -1,32 +1,57 @@
 import React from "react";
-import { signInWithEmailAndPassword, signOut, onAuthStateChanged } from "firebase/auth";
+import {
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+} from "firebase/auth";
 import SigninView from "../views/signinView";
 import { useRouter } from "next/router";
 
-export default function SigninPresenter({ auth, model }) {
+export default function SigninPresenter({ auth, model, register }) {
   const [email, setEmail] = React.useState("");
   const [password, setPassword] = React.useState("");
   const [userData, setUserData] = React.useState("");
   const [userError, setUserError] = React.useState("");
   const [loading, setLoading] = React.useState(false);
   const router = useRouter();
+  const submitEmailAndPassword = register
+    ? createUserWithEmailAndPassword
+    : signInWithEmailAndPassword;
+
+  React.useEffect(() => {
+    // At loading stage reset any old errors
+    if (loading) setUserError(null);
+  }, [loading]);
+
+  React.useEffect(() => {
+    onAuthStateChanged(
+      auth,
+      (user) => {
+        setUserData(user);
+      },
+      (error) => {
+        setUserError(error);
+      }
+    );
+  }, []);
 
   return (
     <SigninView
-      headerText={"Sign in with your Vinobo account here:"}
+      register={register}
       errorText={userError?.code}
-      loadingText={loading}
-      submitText={"Sign in"}
+      loading={loading}
       onEmail={(email) => setEmail(email)}
       onPassword={(pw) => setPassword(pw)}
       signInUser={() => {
         setLoading(true);
-        signInWithEmailAndPassword(auth, email, password)
+        submitEmailAndPassword(auth, email, password)
           .then((user) => {
+            console.log("authenticating");
             setLoading(false);
             router.push("/");
           })
           .catch((error) => {
+            console.log("failing to authenticate");
             setLoading(false);
             setUserError(error);
           });
