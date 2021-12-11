94ab2570230e12906d1adafd5a7457f4e431dc48
jonaro00 <54029719+jonaro00@users.noreply.github.com>
2021-12-11 03:46:02 +0100

    Refactor useModelGetter

--- js/useModelProperty.js
+++ js/useModelProperty.js
@@ -1,20 +1,21 @@
 import React from "react";
 
 /**
- * Custom hook that registers a model attribute that the View is dependent on.
+ * Custom hook that registers a model attribute or getter that the View is dependent on.
  * Registers an observer to recieve updates of the model attribute, and only re-renders
  * when this attribute changed.
  * @param {*} model An observable object.
- * @param {*} propertyName Attribute name in the model.
+ * @param {*} propertyName Attribute or getter method name in the model.
  * @returns The value of the model attribute (stateful).
  */
 export default function useModelProperty(model, propertyName) {
+  const func = typeof model[propertyName] === "function";
   // Use a state in the component
-  const [value, setValue] = React.useState(model[propertyName]);
+  const [value, setValue] = React.useState(func ? model[propertyName]() : model[propertyName]);
   React.useEffect(
     () => {
       // The observer updates the local state
-      const obs = () => setValue(model[propertyName]);
+      const obs = () => setValue(func ? model[propertyName]() : model[propertyName]);
       model.addObserver(obs); // subscribe
       return () => model.removeObserver(obs); // unsubscribe
     },
--- presenters/notesPresenter.js
+++ presenters/notesPresenter.js
@@ -1,11 +1,9 @@
 import NotesView from "../views/notesView";
 import React from "react";
 import useModelProperty from "../js/useModelProperty";
-import useModelGetter from "../js/useModelGetter";
 
 export default function NotesPresenter(props) {
-  const id = useModelProperty(props.model, "currentVideo");
-  const notes = useModelGetter(props.model, "getNotes");
+  const notes = useModelProperty(props.model, "getNotes");
 
   const [query, setQuery] = React.useState("");
 
