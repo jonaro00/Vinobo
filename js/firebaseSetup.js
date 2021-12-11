b3e00713304e76938d8e577ddcbb94695358bc14
Simon Falk <smnfalk@gmail.com>
2021-12-11 19:43:53 +0100

    config values from env vars

--- js/firebaseSetup.js
+++ js/firebaseSetup.js
@@ -1,22 +1,16 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
-try {
-  import { firebaseConfig } from "../js/firebaseConfig.js";
-} catch {
-  // Create the config object from environment variables
-  // For Heroku deployment
-  const firebaseConfig = {
-    apiKey: process.env.apiKey,
-    authDomain: process.env.authDomain,
-    databaseURL: process.env.databaseURL,
-    projectId: process.env.projectID,
-    storageBucket: process.env.storageBucket,
-    messagingSenderId: process.env.messagingSenderId,
-    appId: process.env.appId,
-  };
-}
 
+const firebaseConfig = {
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  databaseURL: process.env.databaseURL,
+  projectId: process.env.projectID,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
+};
 const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
 const auth = getAuth(app);
