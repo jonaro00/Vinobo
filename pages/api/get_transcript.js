20612c7b423d7388cf9fbda3a0bb7fbd12660f39
jonaro00 <54029719+jonaro00@users.noreply.github.com>
2021-12-13 17:52:25 +0100

    API cache first attempt

--- pages/api/get_transcript.js
+++ pages/api/get_transcript.js
@@ -3,10 +3,13 @@ import YoutubeTranscript from "youtube-transcript";
 export default async function handler(req, res) {
   const id = new URL(req.url, `http://${req.headers.host}`).searchParams.get("id");
   return YoutubeTranscript.fetchTranscript(id)
-    .then((data) => ({
-      status: "success",
-      data: res.status(200).json(data),
-    }))
+    .then((data) => {
+      res.setHeader("Cache-Control", "s-maxage=86400");
+      return {
+        status: "success",
+        data: res.status(200).json(data),
+      };
+    })
     .catch((error) => ({
       status: "error",
       data: res.status(500).json(error),
